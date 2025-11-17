3c675f323689f2327263ed14f65269ad09bc1d2a
AlysonEast <a.east.ecology@gmail.com>
2025-11-17 11:15:28 -0500

    updated the slurm submission to handle batch/array processing

---
+++ GenerateDatasets_Slurm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=CrownsDataset
+#SBATCH --time=0:10:00 #1 hour
+#SBATCH --mail-type=ALL
+#SBATCH --output=./outfiles/out_.%j
+#SBATCH --account=PUOM0017
+
+module purge
+module load gcc/12.3.0
+module load R/4.4.0
+module load proj/9.2.1
+module load gdal/3.7.3
+
+# Pass PRODUCT, SITE, and ARRAY_INDEX to R
+Rscript GenerateDatasetsIndv.R "$SLURM_ARRAY_TASK_ID" "$PRODUCT" "$SITE"
